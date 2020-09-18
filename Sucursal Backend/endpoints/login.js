3a89d806bb3a30f94e1fcd2e1a4db27c8bb68e3f
Tribeth Rivas <tribethr@gmail.com>
2020-09-17 19:49:02 -0600

    [fixed] database cors usage

--- Sucursal Backend/endpoints/login.js
+++ Sucursal Backend/endpoints/login.js
@@ -5,6 +5,10 @@ const dbDriver = require('../database/database');
 const db = new dbDriver.Request();
 
 router.get('/', (req, res) => {
+    console.log({
+        user: req.headers.user, 
+        password: req.headers.password
+    });
     db.query('select * from Test', (error, resultSet) => {
         if(!error){
             res.send(resultSet);
--- Sucursal Backend/index.js
+++ Sucursal Backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 
+//To use in localhost
+app.use(cors());
+
 //To parse URL encoded data
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //To parse json data
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
 
 //Routes
 const login = require('./endpoints/login');
